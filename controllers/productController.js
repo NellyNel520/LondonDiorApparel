894e460beb43eac8fbd3046130a572268e4273e8
NellyNel520 <channelharris520@gmail.com>
2023-03-25 17:44:04 -0400

    created get by route and controller for products and product categories

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 const { ProductCategory, Product } = require('../models')
 
+// Create Products & Product Categories
 const createProductCategory = async (req, res) => {
 	try {
 		const category = await new ProductCategory(req.body);
@@ -31,10 +32,66 @@ const createProduct = async (req, res) => {
 	}
 };
 
+// Get ALL Products & Product Categories
+
+const getAllCategories = async (req, res) => {
+	try {
+		const categories = await ProductCategory.find();
+		return res.status(200).json({ categories });
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+const getAllProducts = async (req, res) => {
+	try {
+		const products = await Product.find();
+		return res.status(200).json({ products });
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+// Get Products & Product Categories By ID
+
+const getCategoryById = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const category = await ProductCategory.findById(id);
+		if (category) {
+			return res.status(200).json({ category });
+		}
+		return res.status(404).send('Category with the specified ID does not exists');
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+const getProductById = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findById(id);
+		if (product) {
+			return res.status(200).json({ product });
+		}
+		return res
+			.status(404)
+			.send('Product with the specified ID does not exists');
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+
 
 
 
 module.exports ={
   createProductCategory,
-	createProduct
+	createProduct,
+	getAllCategories,
+	getAllProducts,
+	getCategoryById,
+	getProductById
+
 }
--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -4,4 +4,13 @@ const controllers = require("../controllers/productController");
 Router.post('/category', controllers.createProductCategory)
 Router.post('/addProduct', controllers.createProduct)
 
+// Get All (Read)
+Router.get('/category', controllers.getAllCategories)
+Router.get('/products', controllers.getAllProducts)
+
+
+// Get by ID
+Router.get('/category/:id', controllers.getCategoryById)
+Router.get('/products/:id', controllers.getProductById)
+
 module.exports = Router;
