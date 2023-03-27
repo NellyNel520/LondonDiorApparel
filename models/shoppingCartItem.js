590053fc67464017b165362b842c8f5374866d38
NellyNel520 <channelharris520@gmail.com>
2023-03-27 14:50:18 -0400

    restructred order model

--- models/shoppingCartItem.js
+++ models/shoppingCartItem.js
@@ -1,15 +1,55 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const ShoppingCartItem = new Schema(
-    {
-      customerId: {type: Schema.Types.ObjectId, ref: 'User'},
-      productId: {type: Schema.Types.ObjectId, ref: 'Product'},
-      quantity: { type: Number, required: true },
-      price: { type: Number, required: true }
+const Order = new Schema(
+  {
+    orderItems: [
+      {
+        slug: { type: String, required: true },
+        name: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {type: Schema.Types.ObjectId, ref: 'Product', required: true},
+      },
+    ],
+    shippingAddress: {
+      fullName: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+      location: {
+        lat: Number,
+        lng: Number,
+        address: String,
+        name: String,
+        vicinity: String,
+        googleAddressId: String,
+      },
     },
-    { timestamps: true }
+    paymentMethod: { type: String, required: true },
+    paymentResult: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
+    },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
   )
 
 
-module.exports = ShoppingCartItem
+module.exports = Order
+
