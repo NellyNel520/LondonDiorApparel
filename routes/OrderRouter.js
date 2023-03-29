79104dec9ecf483b84282f8d3796028d1ae80e30
NellyNel520 <channelharris520@gmail.com>
2023-03-29 16:26:29 -0400

    created order controller and router

---
+++ routes/OrderRouter.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const controllers = require("../controllers/OrderController");
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const Router = require("express").Router();
 const ProductRouter = require("./ProductRouter")
 const AuthRouter = require("./AuthRouter")
+const OrderRouter = require("./OrderRouter")
 
 
 
@@ -9,5 +10,6 @@ Router.get('/', (req, res) => res.send('This is root!'))
 
 Router.use("/product", ProductRouter)
 Router.use("/auth", AuthRouter)
+Router.use("/order", OrderRouter)
 
 module.exports = Router;
