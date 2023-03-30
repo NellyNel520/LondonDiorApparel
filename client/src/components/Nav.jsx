2f5cf3676da8db0ac63ab5f65f69639d0bd51d4f
NellyNel520 <channelharris520@gmail.com>
2023-03-30 15:16:34 -0400

    added sign out button to app

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,48 +1,72 @@
 import { Link } from 'react-router-dom'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PersonIcon from '@mui/icons-material/Person';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import PersonIcon from '@mui/icons-material/Person'
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 
 const Nav = ({ user, handleLogOut }) => {
-
-
 	let userOptions
 	if (user) {
 		userOptions = (
 			<nav>
-				<Link to="/cart"><ShoppingCartIcon /></Link>
-				<Link to="/about">About Us</Link>
-				<Link to="/products">Products</Link>
-				<Link to="/">Home</Link>
-				<Link onClick={handleLogOut} to="/">
-					Sign Out
-				</Link>
-				<Link>Welcome {user.email}!</Link>
-				<div className="logo">
+				<div className="flex w-full flex-wrap items-center justify-between">
+				<div className='"flex-start flex-wrap items-center justify-between px-6"'>
 					<img
+						className="mt-[0.5rem] ml-0 w-[25%]"
 						alt="logo"
 						src="https://i.postimg.cc/SKTchvDq/LDA-Logo-Blue3.png"
 					/>
+          <h3 className='pl-[35rem] pt-[2rem]'>Welcome {user.email}!</h3>
 				</div>
+				<div className=" "></div>
+				<div className="flex-end basis-[100%] items-center lg:!flex lg:basis-auto">
+          <Link onClick={handleLogOut} to="/">
+            <ExitToAppOutlinedIcon />
+          </Link>
+					<Link to="/">
+						<PersonIcon />
+					</Link>
+					<Link to="/cart">
+						<ShoppingCartIcon />
+					</Link>
+					<Link>
+						<FavoriteBorderOutlinedIcon />
+					</Link>
+					<Link to="/">Home</Link>
+					<Link to="/products">Products</Link>
+					<Link to="/about">About Us</Link>
+				</div>
+			</div>
 			</nav>
 		)
 	}
 
 	const publicOptions = (
 		<nav>
-			<Link to="/cart"><ShoppingCartIcon /></Link>
-      <Link to="/register"><PersonIcon/></Link>
-      <Link><FavoriteBorderOutlinedIcon/></Link>
-			<Link to="/about">About Us</Link>
-			<Link to="/signIn">Sign In</Link>
-			<Link to="/products">Products</Link>
-			<Link to="/">Home</Link>
-			<div className="logo">
-				<img
-					alt="logo"
-					src="https://i.postimg.cc/SKTchvDq/LDA-Logo-Blue3.png"
-				/>
+			<div className="flex w-full flex-wrap items-center justify-between">
+				<div className='"flex-start flex-wrap items-center justify-between px-6"'>
+					<img
+						className="mt-[0.5rem] ml-0 w-[25%]"
+						alt="logo"
+						src="https://i.postimg.cc/SKTchvDq/LDA-Logo-Blue3.png"
+					/>
+				</div>
+				<div className=" "></div>
+				<div className="flex-end basis-[100%] items-center lg:!flex lg:basis-auto">
+					<Link to="/register">
+						<PersonIcon />
+					</Link>
+					<Link to="/cart">
+						<ShoppingCartIcon />
+					</Link>
+					<Link>
+						<FavoriteBorderOutlinedIcon />
+					</Link>
+					<Link to="/">Home</Link>
+					<Link to="/products">Products</Link>
+					<Link to="/about">About Us</Link>
+				</div>
 			</div>
 		</nav>
 	)
